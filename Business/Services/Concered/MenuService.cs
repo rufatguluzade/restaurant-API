f8a0d59d5312ac21aa2b467a8cd9dcbd61256c74
rufff1 <rufatzg@code.edu.az>
2024-04-25 15:07:50 +0400

    Menu Table End

--- Business/Services/Concered/MenuService.cs
+++ Business/Services/Concered/MenuService.cs
@@ -1,7 +1,18 @@
-﻿using Business.DTOs.Common;
+﻿using AutoMapper;
+using Business.DTOs.Common;
 using Business.DTOs.Menu.Request;
 using Business.DTOs.Menu.Response;
+using Business.DTOs.RestaurantLocations.Location.Response;
+using Business.Exceptions;
 using Business.Services.Abstraction;
+using Business.Validators.Menu;
+using Business.Validators.Restaurant.Location;
+using Common.Entities;
+using DataAccess.Repositories.Abstract;
+using DataAccess.Repositories.Concrete;
+using DataAccess.UnitOfWork;
+using DocumentFormat.OpenXml.Spreadsheet;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,30 +24,137 @@ namespace Business.Services.Concered
     public class MenuService : IMenuService
     {
 
+        private readonly IMenuRepository _menuRepository;
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IMapper _mapper;
 
-        public Task<Response> CreateAsync(MenuCreateDto model)
+
+
+        public MenuService(IMenuRepository menuRepository, IUnitOfWork unitOfWork, IMapper mapper)
         {
-            throw new NotImplementedException();
+            _menuRepository = menuRepository;
+            _unitOfWork = unitOfWork;
+            _mapper = mapper;
+
+
         }
 
-        public Task<Response> DeleteAsync(int id)
+        public async Task<Response> CreateAsync(MenuCreateDto model)
         {
-            throw new NotImplementedException();
+            var result =await new MenuCreateDtoValidator().ValidateAsync(model);
+
+            if(!result.IsValid){
+                throw new ValidationException(result.Errors);
+            }
+
+            if (await _menuRepository.IsExistAsync(m=> m.Name == model.Name))
+            {
+                throw new ValidationException("bu adda menu movcuddur");
+            }
+            var menu = _mapper.Map<Menu>(model);
+
+            await _menuRepository.CreateAsync(menu);
+            await _unitOfWork.CommitAsync();
+
+            return new Response
+            {
+
+                Message = "menu ugurla yaradildi"
+            };
+
         }
 
-        public Task<Response<List<MenuResponseDto>>> GetAllAsync(string? search)
+        public async Task<Response> DeleteAsync(int id)
         {
-            throw new NotImplementedException();
+            var menu = await _menuRepository.GetAsync(id);
+
+
+            if (menu == null) throw new ValidationException($" Id-si {id} olan menu movcud deyil");
+
+            _menuRepository.Delete(menu);
+            await _unitOfWork.CommitAsync();
+
+            return new Response
+            {
+                Message = "menu ugurla silindi"
+            };
         }
 
-        public Task<Response<MenuResponseDto>> GetAsync(int id)
+        public async Task<Response<List<MenuResponseDto>>> GetAllAsync(string? search)
         {
-            throw new NotImplementedException();
+
+            var menus = await _menuRepository.GetFiltered(
+         b => search != null ? b.Name.Contains(search) : true,
+         isTracking: false).ToListAsync();
+
+            if (menus is null)
+            {
+
+                throw new NotFoundException("Hec bir menu tapilmadi");
+            }
+
+            return new Response<List<MenuResponseDto>>
+            {
+                Data = _mapper.Map<List<MenuResponseDto>>(menus),
+                Message = "data ugurla getirildi"
+            };
         }
 
-        public Task<Response> UpdateAsync(int id, MenuUpdateDto model)
+        public async Task<Response<MenuResponseDto>> GetAsync(int id)
         {
-            throw new NotImplementedException();
+            var menu = await _menuRepository.GetAsync(id);
+
+            if (menu is null)
+            {
+                throw new NotFoundException("Hec bir menu tapilmadi");
+            }
+
+            return new Response<MenuResponseDto>
+            {
+                Data = _mapper.Map<MenuResponseDto>(menu),
+                Message = "data ugurla getirildi"
+            };
+        }
+
+        public async Task<Response> UpdateAsync(int id, MenuUpdateDto model)
+        {
+            var result = await new MenuUpdateDtoValidator().ValidateAsync(model);
+
+            if (!result.IsValid)
+            {
+                throw new ValidationException(result.Errors);
+            }
+
+         
+
+            var existMenu = await _menuRepository.GetAsync(id);
+
+            if (await _menuRepository.IsExistAsync(m => m.Name == existMenu.Name))
+            {
+                throw new ValidationException("bu adda menu movcuddur");
+            }
+            if (existMenu is null)
+            {
+                throw new NotFoundException("menu tapilmadi");
+            }
+
+            _mapper.Map(model, existMenu);
+
+
+
+            existMenu.Name = existMenu.Name;
+            existMenu.ModifiedDate = DateTime.Now;
+
+
+
+
+            _menuRepository.Update(existMenu);
+            await _unitOfWork.CommitAsync();
+
+            return new Response<MenuResponseDto>
+            {
+                Message = "ugurla update olundu"
+            };
         }
     }
 }
---
+++ Presentation/Controllers/MenuController.cs
@@ -0,0 +1,100 @@
+﻿using Business.DTOs.Common;
+using Business.DTOs.Menu.Request;
+using Business.DTOs.Menu.Response;
+using Business.Services.Abstraction;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Presentation.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MenuController : ControllerBase
+    {
+
+        private readonly IMenuService _menuService;
+
+        public MenuController(IMenuService menuService)
+        {
+            _menuService = menuService;
+        }
+
+        #region Documentation
+        /// <summary>
+        /// Menu yaradılması üçün
+        /// </summary>
+        /// <param name="model"></param>
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(Response))]
+        [ProducesResponseType(StatusCodes.Status400BadRequest, Type = typeof(Response))]
+        #endregion
+        [HttpPost("Create")]
+        // [Authorize(Roles = "Admin", AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+
+        public async Task<Response> CreateAsync([FromBody] MenuCreateDto model)
+        {
+            return await _menuService.CreateAsync(model);
+        }
+
+
+        #region Documentation
+        /// <summary>
+        /// Menu redakte etmek üçün
+        /// </summary>
+        /// <param name="model"></param>
+        [ProducesResponseType(StatusCodes.Status404NotFound, Type = typeof(Response))]
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(Response))]
+        [ProducesResponseType(StatusCodes.Status400BadRequest, Type = typeof(Response))]
+        #endregion
+        [HttpPut("Update")]
+        public async Task<Response> UpdateAsync(int id, [FromBody] MenuUpdateDto model)
+        {
+            return await _menuService.UpdateAsync(id, model);
+        }
+
+
+
+        #region Documentation
+        /// <summary>
+        ///  Menu silinməsi
+        /// </summary>
+        /// <param name="id"></param>
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(Response))]
+        [ProducesResponseType(StatusCodes.Status404NotFound, Type = typeof(Response))]
+        #endregion
+        [HttpDelete("Delete")]
+        public async Task<Response> DeleteAsync(int id)
+        {
+            return await _menuService.DeleteAsync(id);
+        }
+
+
+
+        #region Documentation
+        /// <summary>
+        ///  Menu id parametrinə görə götürülməsi üçün
+        /// </summary>
+        /// <param name="id"></param>
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(Response<MenuResponseDto>))]
+        [ProducesResponseType(StatusCodes.Status404NotFound, Type = typeof(Response))]
+        #endregion
+        [HttpGet("GetById")]
+        public async Task<Response<MenuResponseDto>> GetAsync(int id)
+        {
+            return await (_menuService.GetAsync(id));
+        }
+
+
+
+        #region Documentation
+        /// <summary>
+        /// Menu siyahısını götürmək üçün
+        /// </summary>
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(Response<List<MenuResponseDto>>))]
+        #endregion
+        [HttpGet()]
+        public async Task<Response<List<MenuResponseDto>>> GetAllAsync([FromQuery] string? search)
+        {
+            return await _menuService.GetAllAsync(search);
+        }
+    }
+}
